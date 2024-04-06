06fd786a234e34a4fc0b105e0ee8caeb754024dc
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2024-04-06 16:01:05 -0400

    login edits

--- myMatch/src/components/LoginSignUp/LoginSignUp.jsx
+++ myMatch/src/components/LoginSignUp/LoginSignUp.jsx
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import emailIcon from "../assets/email.png";
 import passwordIcon from "../assets/password.png";
 import profileIcon from "../assets/person.png";
 
 export default function LoginSignUp() {
-  const [action, setAction]
+  const [action, setAction] = useState("Sign Up")
 
   return (
     <div className="logContainer">
       <div className="logHeader">
-        <div className="logText">Sign Up</div>
+        <div className="logText">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="logInputs">
-          <div className="logInput">
+        {action ==="Log in"? <></>:
+        <div className="logInput">
             <img src={profileIcon} alt="" />
             <input type="text" placeholder="username"/>
           </div>
+          }
 
           <div className="logInput">
             <img src={emailIcon} alt="" />
@@ -30,14 +32,16 @@ export default function LoginSignUp() {
           </div>
         
       </div>
+      {action ==="Sign Up"? <></>:
       <div className="forgot-password">
         Forgot password? <span>click here</span>
       </div>
+      }
 <div className="submit-container">
-    <div className="logSubmit">
+    <div className={action === "Log in"? "logSubmit gray": "logSubmit"} onClick={()=>{setAction("Sign Up")}}>
       Sign Up
     </div>
-    <div className="logSubmit">
+    <div className={action === "Sign Up"? "logSubmit gray": "logSubmit"} onClick={()=>{setAction("Log in")}}>
       Log in
     </div>
 </div>

17cc8a85a32248db4c68f6703c573b18a416a783
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2024-04-26 01:59:39 -0400

    showcase edits

--- myMatch/src/components/showcase.jsx
+++ myMatch/src/components/showcase.jsx
@@ -4,7 +4,45 @@ import React, { Component } from 'react'
 export default function Showcase() {
   return (
     <div>
-        <h1>showcase</h1>
+        <div>
+            <h1>showcase</h1>
+        <div className='buttonContainer'> <button className='findMatchButton'>Find Match</button></div>
+
+        <div className='showcaseContainer'>
+            <h3>Best Match</h3>
+            <div className="user1Stats">
+                <h2 className='name' id='user1Name'>User1</h2>
+
+                <div className='attributes'><h4>attributes</h4>
+                <ul>
+                    <li>Preferences
+                        <ul>
+                            <li>communication<span id=''>mic</span></li>
+                            <li>Playstyle<span id=''>DPS</span></li>
+                        </ul>
+                    </li>
+                    <li>Attributes
+                        <ul>
+                            <li>communication<span id=''>mic</span></li>
+                            <li>Playstyle<span id=''>DPS</span></li>
+                            <li>Playstyle<span id=''>DPS</span></li>
+                        </ul>
+                    </li>
+                    <li>Preferences
+                        <ul>
+                            <li>communication<span id=''>mic</span></li>
+                            <li>Playstyle<span id=''>DPS</span></li>
+                        </ul>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            <div className="user2Stats">
+
+            </div>
+        </div>
+        </div>
+        
         
       </div>
   )
